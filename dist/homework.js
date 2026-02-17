393f401ef84c19e686ec3ff05b7267273a738711
Vladislav <stran.rus228@yandex.ru>
2026-02-17 16:54:49 +0300

    3

--- dist/homework.js
+++ dist/homework.js
@@ -1,30 +1,50 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const MeDev = {
-    level: "junior",
-    login: "Vlad",
-    skills: ["HTML", "CSS", "JS", "TS"],
+    level: 'junior',
+    login: 'Vlad',
+    skills: ['HTML', 'CSS', 'JS', 'TS'],
     isLookingForJob: true,
 };
-const apple = {
-    id: 1,
-    price: 100,
-    title: 'яблоко'
-};
-console.log(apple.price);
-const myCart = {
-    product: {
-        id: 1,
-        price: 100,
-        title: "яблоко",
+// const apple: IProduct = {
+//   id: 1,
+//   price: 100,
+//   title: 'яблоко',
+// };
+// console.log(apple.price);
+// let myCartTest: ICartEntry = {
+//   product: {
+//     id: 1,
+//     price: 100,
+//     title: 'яблоко',
+//   },
+//   quantity: 1,
+// };
+// console.log(myCartTest.product.price);
+let myCart = [
+    {
+        product: {
+            id: 1,
+            price: 100,
+            title: 'яблоко',
+        },
+        quantity: 6,
     },
-    quantity: 1,
-};
-console.log(myCart.product.price);
+    {
+        product: {
+            id: 2,
+            price: 300,
+            title: 'колбаса',
+        },
+        quantity: 1,
+    },
+];
+myCart.push({ product: { id: 3, price: 20, title: 'жвачка' }, quantity: 2 });
 function calculateTotal(cart) {
     let total = 0;
-    // for (const price: number of cart) {
-    //   total += cart
-    // }
+    for (const priceProduct of cart) {
+        total += priceProduct.product.price * priceProduct.quantity;
+    }
     return total;
 }
+console.log(calculateTotal(myCart));
--- src/homework.ts
+++ src/homework.ts
@@ -28,32 +28,51 @@ interface ICartEntry {
   quantity: number;
 }
 
-const apple: IProduct = {
-  id: 1,
-  price: 100,
-  title: 'яблоко',
-};
-console.log(apple.price);
+// const apple: IProduct = {
+//   id: 1,
+//   price: 100,
+//   title: 'яблоко',
+// };
+// console.log(apple.price);
 
-let myCartTest: ICartEntry = {
-  product: {
-    id: 1,
-    price: 100,
-    title: 'яблоко',
+// let myCartTest: ICartEntry = {
+//   product: {
+//     id: 1,
+//     price: 100,
+//     title: 'яблоко',
+//   },
+//   quantity: 1,
+// };
+// console.log(myCartTest.product.price);
+
+let myCart: ICartEntry[] = [
+  {
+    product: {
+      id: 1,
+      price: 100,
+      title: 'яблоко',
+    },
+    quantity: 6,
   },
-  quantity: 1,
-};
-console.log(myCartTest.product.price);
+  {
+    product: {
+      id: 2,
+      price: 300,
+      title: 'колбаса',
+    },
+    quantity: 1,
+  },
+];
 
-
+myCart.push({product: { id: 3, price: 20, title: 'жвачка'}, quantity: 2})
 
 function calculateTotal(cart: ICartEntry[]): number {
   let total: number = 0;
-  // for (const price: number of cart) {
-  //   total += cart
-  // }
+  for (const priceProduct of cart) {
+    total += priceProduct.product.price * priceProduct.quantity
+  }
 
   return total;
 }
 
-console.log();
+console.log(calculateTotal(myCart));
